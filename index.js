bfd94c9a3a7a7ab68a698064a7951f73e73bd45e
chim7a <chimalawrence11@gmail.com>
2024-11-19 11:58:55 +0100

    first commit

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ async function getData() {
   }
 }
 
-getData();
+// getData();
 
 function examplePromise(params) {
   return new Promise((resolve, reject) => {
+    // Perform long task here.
     const response = "success";
-    // Based on th re
+    // Based on th result of the long operation will determine if promise
+    // was successful or rejected
     if (response === "success") {
       resolve("Hey Big head");
     } else {
@@ -25,6 +27,48 @@ function examplePromise(params) {
   });
 }
 
-examplePromise()
-  .then((data) => console.log("Completed", data))
-  .catch((error) => console.log("error", error));
+// examplePromise()
+//   .then((data) => console.log("Completed", data))
+//   .catch((error) => console.log("error", error));
+
+function smallPromiseOne(params) {
+  return new Promise((resolve, reject) => {
+    const response = "success";
+    if (response === "success") {
+      resolve("Promise one was successful");
+    } else {
+      reject("Promise one was rejected");
+    }
+  });
+}
+
+function smallPromiseTwo(params) {
+  return new Promise((resolve, reject) => {
+    const response = "success";
+    // Based on th re
+    if (response === "success") {
+      resolve("Promise Two was successful");
+    } else {
+      reject("Promise Two was rejected");
+    }
+  });
+}
+
+// Executing multiple promises at same time.
+// when using promise.all(), if one is affected, it becomes a problem for others
+// Promise.all([smallPromiseOne(), smallPromiseTwo()])
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+// when using promise.allSettled(), if one is affected, it is not a problem for others
+Promise.allSettled([smallPromiseOne(), smallPromiseTwo()])
+  .then((data) => console.log("From settled", data))
+  .catch((error) => console.log("From settled", error));
+// when using promise.race(), returns the fastest to resolve or rejected
+Promise.race([getData(), smallPromiseOne(), smallPromiseTwo()])
+  .then((data) => console.log("From race", data))
+  .catch((error) => console.log("From race", error));
+// when using promise.any(), returns the fastest to get resolved.
+Promise.any([getData(), smallPromiseOne(), smallPromiseTwo()])
+  .then((data) => console.log("From any", data))
+  .catch((error) => console.log("From any", error));
